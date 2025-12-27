4389d597606836e80a7393fb1ebde8cde9182b04
Arseniy <palaznikarseni@gmail.com>
2025-12-28 01:42:31 +0300

    fix build

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#317EFB" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+    </>
+  );
+}
