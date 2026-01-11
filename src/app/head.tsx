646bb14821801472de4d0dcdb779d05780cce072
Arseniy <palaznikarseni@gmail.com>
2026-01-12 02:11:35 +0300

    Add notifications

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,13 +2,16 @@ export default function Head() {
   return (
     <>
       <link rel="manifest" href="/manifest.json" />
-      <meta name="theme-color" content="#317EFB" />
+
+      <meta name="theme-color" content="#ffffff" />
 
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta
         name="apple-mobile-web-app-status-bar-style"
         content="black-translucent"
       />
+
+      <meta name="mobile-web-app-capable" content="yes" />
 
       <link
         rel="apple-touch-icon"
